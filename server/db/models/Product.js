eed06251a197aab5963d0412b68163680e0742ed
efb <evanbarden@gmail.com>
2022-08-02 15:17:05 -0400

    ads db models for user and product

---
+++ server/db/models/Product.js
@@ -0,0 +1,40 @@
+const Sequelize = require("sequelize");
+const db = require("../db");
+
+const Product = db.define("product", {
+  name: {
+    type: Sequelize.STRING,
+    unique: true,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
+  },
+  price: {
+    type: Sequelize.FLOAT,
+    allowNull: false,
+    validate: {
+      min: 0.0,
+    },
+  },
+  desc: {
+    type: Sequelize.TEXT,
+  },
+  img: {
+    type: Sequelize.STRING,
+    defaultValue: "https://emojiisland.com/products/neutral-face-emoji-icon",
+  },
+  quantity: {
+    type: Sequelize.INTEGER,
+    defaultValue: 0,
+    validate: {
+      min: 0,
+    },
+  },
+  onSale: {
+    type: Sequelize.BOOLEAN,
+    defaultValue: false,
+  },
+});
+
+module.exports = Product;
