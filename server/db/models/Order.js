53a1365e004c82177e8d748a4185b00117386f12
efb <evanbarden@gmail.com>
2022-08-03 15:48:35 -0400

    adds Order model to db

---
+++ server/db/models/Order.js
@@ -0,0 +1,11 @@
+const Sequelize = require("sequelize");
+const db = require("../db");
+
+const Order = db.define("order", {
+  status: {
+    type: Sequelize.ENUM("active", "closed"),
+    defaultValue: "active",
+  },
+});
+
+module.exports = Order;
