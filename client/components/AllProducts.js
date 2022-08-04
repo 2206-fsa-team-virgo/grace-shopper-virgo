bd875ba442977a206f55adcab6b26599b84ffa6d
Daniel Jacobson <danieljacobson@Daniels-MacBook-Pro-6.local>
2022-08-04 14:12:29 -0500

    Got local storage working with dummy data

--- client/components/AllProducts.js
+++ client/components/AllProducts.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { useHistory } from 'react-router-dom';
+
 
 
 const products = [
@@ -44,24 +44,32 @@ export const AllProducts = (props) => {
   const saveLocalCart = (item) => {
     let cart;
     if (localStorage.getItem("cart") === null) {
+      //If no local storage present, create a cart
       cart = [];
     } else {
+      //If there is, parse the local storage string, and assign cart to parsed string (which is now the cart array)
       cart = JSON.parse(localStorage.getItem("cart"));
     }
+    //Push the item onto the server (our computers) cart array
     cart.push(item)
+    //Update the cart on the web's local storage
     localStorage.setItem("cart", JSON.stringify(cart))
   };
   return (
     <div>
       <h3>Shop Emojis!</h3>
-      {products.map((product) => (
+      {products.map((product) => {
+        const caller = () => {
+          saveLocalCart(product)
+        }
+        return (
         <div>
           <img src={product.img} />
           <div>Name: {product.name}</div>
           <div>Price: ${product.price / 100}</div>
-          <button onClick={saveLocalCart(product)}>Add to cart</button>
+          <button onClick={caller}>Add to cart</button>
         </div>
-      ))}
+      )})}
       <div>{/*  Yellow bar with icons here! */}</div>
     </div>
   );
