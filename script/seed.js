8b30083e9da014d9cae300fa128d14f6e55cc762
efb <evanbarden@gmail.com>
2022-08-03 21:42:40 -0400

    adds orders to seed file and associates orders with users in seed

--- script/seed.js
+++ script/seed.js
@@ -2,7 +2,7 @@
 
 const {
   db,
-  models: { User, Product },
+  models: { User, Product, Order },
 } = require("../server/db");
 const axios = require("axios");
 
@@ -62,45 +62,105 @@ async function seed() {
   ]);
 
   console.log(`seeded ${users.length} users`);
-  console.log(`seeded successfully`);
+  console.log(`seeded users successfully`);
 
   const getEmoji = async () => {
     let res = await axios.get(emojiUrl);
-    // .then(console.log);
-    // console.log(res.data);
-    return res;
+    return res.data;
   };
 
   const emojiArray = await getEmoji();
 
-  const products = await Promise.all([
+  const productArray = emojiArray.map((emoji) => {
     Product.create({
-      name: "smiley",
-      price: 10.5,
-      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      name: emoji.unicodeName,
+      price: 2,
+      desc: emoji.character,
       quantity: 10,
+    });
+  });
+
+  const products = await Promise.all(productArray);
+
+  // const products = await Promise.all([
+  //   Product.create({
+  //     name: "smiley",
+  //     price: 10.5,
+  //     desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  //     quantity: 10,
+  //   }),
+  // ]);
+
+  console.log(`seeded ${products.length} products`);
+  // console.log(products);
+  console.log(`seeded products successfully`);
+
+  const orders = await Promise.all([
+    Order.create({
+      status: "active",
     }),
-    Product.create({
-      name: "smiley wink",
-      price: 9.5,
-      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-      quantity: 3,
+    Order.create({
+      status: "active",
     }),
-    Product.create({
-      name: "frown face",
-      price: 11.5,
-      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    Order.create({
+      status: "active",
     }),
-    Product.create({
-      name: "squinty smile",
-      price: 5.5,
-      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-      quantity: 1,
+    Order.create({
+      status: "active",
+    }),
+    Order.create({
+      status: "closed",
+    }),
+    Order.create({
+      status: "closed",
+    }),
+    Order.create({
+      status: "closed",
     }),
   ]);
 
-  console.log(`seeded ${products.length} responses`);
-  console.log(`seeded successfully`);
+  console.log(`seeded ${orders.length} orders`);
+  // console.log(orders);
+  console.log(`seeded orders successfully`);
+
+  // associate orders with users
+  orders[0].setUser(users[0]);
+  orders[1].setUser(users[1]);
+  orders[2].setUser(users[2]);
+  orders[3].setUser(users[3]);
+  orders[4].setUser(users[1]);
+  orders[5].setUser(users[4]);
+  orders[6].setUser(users[5]);
+
+  // add some products to orders
+  // console.log(products);
+  // await products[0].addOrder(orders[0]);
+  // await products[1].addOrder(orders[0]);
+  // await products[2].addOrder(orders[0]);
+
+  // await products[0].addOrder(orders[1]);
+  // await products[3].addOrder(orders[1]);
+  // await products[4].addOrder(orders[1]);
+
+  // await products[5].addOrder(orders[2]);
+  // await products[6].addOrder(orders[2]);
+  // await products[7].addOrder(orders[2]);
+
+  // await products[8].addOrder(orders[3]);
+  // await products[6].addOrder(orders[3]);
+  // await products[9].addOrder(orders[3]);
+
+  // await products[10].addOrder(orders[4]);
+  // await products[11].addOrder(orders[4]);
+  // await products[12].addOrder(orders[4]);
+
+  // await products[12].addOrder(orders[5]);
+  // await products[13].addOrder(orders[5]);
+  // await products[14].addOrder(orders[5]);
+
+  // await products[15].addOrder(orders[6]);
+  // await products[16].addOrder(orders[6]);
+  // await products[14].addOrder(orders[6]);
 
   return {
     users: {
@@ -112,6 +172,15 @@ async function seed() {
       kevin: users[5],
     },
     products: products.length,
+    orders: {
+      order1: orders[0],
+      order2: orders[1],
+      order3: orders[2],
+      order4: orders[3],
+      order5: orders[4],
+      order6: orders[5],
+      order7: orders[6],
+    },
   };
 }
 
