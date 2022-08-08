3a2797020de880878ed4b12f2210965984741773
Daniel Jacobson <danieljacobson@Daniels-MacBook-Pro-6.local>
2022-08-08 14:00:10 -0500

    Products post route up

--- server/api/products.js
+++ server/api/products.js
@@ -21,3 +21,11 @@ router.get("/:productId", async (req, res, next) => {
     next(err);
   }
 });
+
+router.post('/', async (req, res, next) => {
+  try {
+    res.status(201).send(await Product.create(req.body));
+  } catch (error) {
+    next(error);
+  }
+});
