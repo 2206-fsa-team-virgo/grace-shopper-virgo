a1d398e7fd3afcab0ab98212180932f01479ddfd
efb <evanbarden@gmail.com>
2022-08-09 23:06:31 -0400

    finalizes backend routes for cart

--- server/api/orders.js
+++ server/api/orders.js
@@ -42,8 +42,8 @@ router.get("/", requireToken, async (req, res, next) => {
   }
 });
 
-// edit current cart >>> needs logic for removing item?
-router.put("/", requireToken, async (req, res, next) => {
+// add item to cart
+router.post("/", requireToken, async (req, res, next) => {
   try {
     // make sure this is a logged in user
     if (req.user) {
@@ -60,62 +60,40 @@ router.put("/", requireToken, async (req, res, next) => {
       // console.log("orderId", orderId);
 
       // convert req.body (online cart) into array
-      const webCart = Object.values(req.body);
+      const itemToAdd = req.body;
       // console.log("webCart", webCart);
 
       // for each item in the webCart, we want to create a row in order_products
       // or update the quantity and unit_price accordingly
-      await webCart.forEach(async (item) => {
-        console.log("ITEM ID-->", item.id, typeof item.id);
-        // see if the product is already in the db cart
-        const existingItem = await Order_Product.findOne({
-          where: {
-            orderId: orderId,
-            productId: item.id,
-          },
-        });
-        // console.log("existingItem", existingItem);
-        // console.log(typeof existingItem);
-        if (existingItem === null) {
-          // console.log("ITEM ID IN NEW-->", item.id);
-          await Order_Product.create({
-            productId: item.id,
-            orderId: orderId,
-            unit_price: parseFloat(item.price),
-            quantity: item.qty,
-          });
-          // console.log("newItem-->", newItem);
-        } else {
-          // console.log("ITEM ID IN UPDATE-->", item.id);
-          if (item.qty <= 0) {
-            await existingItem.destory();
-          } else {
-            await existingItem.update({
-              unit_price: parseFloat(item.price),
-              quantity: item.qty,
-            });
-          }
-          // console.log("updatedItem-->", updatedItem);
-        }
-      });
 
-      let updatedOrder = await Order.findOne({
-        include: {
-          model: Product,
-          attributes: ["id", "name", "desc", "onSale"],
-          through: {
-            attributes: ["quantity", "unit_price"],
-          },
-        },
+      console.log("ITEM ID-->", itemToAdd.id, typeof itemToAdd.id);
+      // see if the product is already in the db cart
+      const existingItem = await Order_Product.findOne({
         where: {
-          id: orderId,
+          orderId: orderId,
+          productId: itemToAdd.id,
         },
-        attributes: ["id", "status"],
       });
+      // console.log("existingItem", existingItem);
+      // console.log(typeof existingItem);
+      if (existingItem === null) {
+        // console.log("ITEM ID IN NEW-->", item.id);
+        await Order_Product.create({
+          productId: itemToAdd.id,
+          orderId: orderId,
+          unit_price: parseFloat(itemToAdd.price),
+          quantity: itemToAdd.qty,
+        });
+      } else {
+        // console.log("ITEM ID IN UPDATE-->", item.id);
+        await existingItem.update({
+          unit_price: parseFloat(itemToAdd.price),
+          quantity: itemToAdd.qty,
+        });
+        // console.log("updatedItem-->", updatedItem);
+      }
 
-      console.log(updatedOrder);
-
-      res.status(202).send(updatedOrder);
+      res.sendStatus(202);
     } else {
       res.sendStatus(404);
     }
@@ -148,6 +126,89 @@ router.put("/close", requireToken, async (req, res, next) => {
       newOrder.setUser(await User.findByPk(userId));
 
       res.status(201).send(newOrder);
+    } else {
+      res.sendStatus(404);
+    }
+  } catch (err) {
+    next(err);
+  }
+});
+
+// edit current cart's product(s)
+router.put("/:productId", requireToken, async (req, res, next) => {
+  try {
+    // make sure this is a logged in user
+    if (req.user) {
+      // get the user's cart so we can extract orderId
+      const order = await Order.findOne({
+        where: {
+          userId: req.user.id,
+          status: "active",
+        },
+        attributes: ["id"],
+      });
+      // set orderId from order above
+      const orderId = order.id;
+      // console.log("orderId", orderId);
+
+      // convert req.body (edited item) into array
+      const editedItem = req.body;
+      // console.log("editedProduct", editedProduct);
+
+      const existingItem = await Order_Product.findOne({
+        where: {
+          orderId: orderId,
+          productId: req.params.productId,
+        },
+      });
+
+      if (editedItem.qty <= 0) {
+        await existingItem.destroy();
+      } else {
+        await existingItem.update({
+          unit_price: parseFloat(editedItem.price),
+          quantity: editedItem.qty,
+        });
+      }
+
+      res.sendStatus(202);
+    } else {
+      res.sendStatus(404);
+    }
+  } catch (err) {
+    next(err);
+  }
+});
+
+// delete all of a product from the current cart
+router.delete("/:productId", requireToken, async (req, res, next) => {
+  try {
+    // make sure this is a logged in user
+    if (req.user) {
+      // get the user's cart so we can extract orderId
+      const order = await Order.findOne({
+        where: {
+          userId: req.user.id,
+          status: "active",
+        },
+        attributes: ["id"],
+      });
+      // set orderId from order above
+      const orderId = order.id;
+      // console.log("orderId", orderId);
+
+      const existingItem = await Order_Product.findOne({
+        where: {
+          orderId: orderId,
+          productId: req.params.productId,
+        },
+      });
+
+      console.log(existingItem);
+
+      await existingItem.destroy();
+
+      res.sendStatus(204);
     } else {
       res.sendStatus(404);
     }
